a01143983181cd2e115991f820b069499492a9cd
PopSurasak <poptyza007@gmail.com>
2026-01-16 14:37:19 +0700

    gg

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+function HomePage() {
+  return (
+  <div><div className="text-center mt-3 p-3">
+    <h1 className="text-4xl bg-blue-300">Welcome to My App</h1>
+    <p className="text-2xl p-3 bg-blue-200">เกย์โหดกระโดดถีบ</p>
+    </div>
+    <div className="text-center text-2xl mt-6 text-amber-300">
+      <h1>บทเรียนที่1 State การเปิด/ปิดหลอกไฟ</h1>
+    </div>
+  </div>
+  );
+}
+
+export default HomePage
